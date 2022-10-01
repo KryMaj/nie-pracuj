e20b87eade5fca7155e391b903de950a9e751f9f
KryMaj <krystianmajda96@gmail.com>
2022-10-01 11:57:49 +0200

    feature/zadanie-1-profile-aplikacji

--- src/main/java/pl/niepracuj/NiePracujApplication.java
+++ src/main/java/pl/niepracuj/NiePracujApplication.java
@@ -2,12 +2,20 @@ package pl.niepracuj;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 
 @SpringBootApplication
-public class NiePracujApplication {
+public class NiePracujApplication extends SpringBootServletInitializer {
 
+    @Override
+    protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+        return application.sources(NiePracujApplication.class);
+    }
     public static void main(String[] args) {
         SpringApplication.run(NiePracujApplication.class, args);
     }
 
 }
+
+
