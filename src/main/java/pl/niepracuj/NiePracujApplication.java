a3d74410e3f50ba20219f3d77e798884126a6f54
KryMaj <krystianmajda96@gmail.com>
2022-10-01 11:06:12 +0200

    first commit

---
+++ src/main/java/pl/niepracuj/NiePracujApplication.java
@@ -0,0 +1,13 @@
+package pl.niepracuj;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NiePracujApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(NiePracujApplication.class, args);
+    }
+
+}
